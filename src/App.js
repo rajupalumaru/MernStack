7f6fd31c637d4a95f53b79a3f3454db91cd4fd57
rajupalumaru <raju.palumaru94@gmail.com>
2022-12-03 12:36:23 +0530

    abcd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 function App() {
+  const [item, setItem] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/gettask')
+      .then(
+        arr => setItem(arr.data)
+      )
+  })
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/addtask', { todo: newTask })
+      .then(
+        arr => setItem(arr.data)
+      )
+  }
+  const deleteHandler = id => {
+    console.log("id",id);
+    axios.delete(`http://localhost:5000/delete/${id}`)
+      .then(
+        arr => setItem(arr.data)
+      )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <form onSubmit={submitHandler}>
+          <input type="text" value={newTask}
+            onChange={(e) => setNewTask(e.target.value)} />
+          <input type='submit' value='sumit' />
+
+        </form><br />
+        {item.map(ele =>
+          <div key={ele._id}>
+            <h3>{ele.todo}</h3>
+            <button onClick={() => deleteHandler(ele._id)}>Delete</button>
+          </div>
+        )}
+      </center>
     </div>
   );
 }
